340da723f69551dfc0c012e6cae1d1844b2ff2e0
naishlinde <77028122+naishlinde@users.noreply.github.com>
2025-09-26 23:53:40 -0400

    Session 1 prompt for Coding Journal #1

---
+++ cj1-1.py
@@ -0,0 +1,11 @@
+def print_naish():
+    print('My name is Naish Linde')
+    print('He/Him')
+    print('My favorite movie is Life of Pi.')
+    print('My favorite food is a cookie, a perfect freshly baked chocolate chip cookie')
+
+def main():
+    print_naish()
+
+if __name__ == '__main__':
+    main()
